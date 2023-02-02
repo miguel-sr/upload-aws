809910c835380175dae325611f14ad875528a16d
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-02-02 20:21:57 -0300

    :sparkles: feat: Save file in mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
+
 import routes from "./routes";
 
 const app = express();
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://localhost:27017/upload-aws").then(() => {
+  console.log("==> MongoDB Connected");
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,14 +2,22 @@ import { Router, Request, Response } from "express";
 import multer from "multer";
 import multerConfig from "./config/multer";
 
+import Post from "./models/post";
+
 const routes = Router();
 
 routes.post(
   "/posts",
   multer(multerConfig).single("file"),
-  (req: Request, res: Response) => {
-    console.log(req.file);
-    return res.send({ Hello: "World" });
+  async (req: Request, res: Response) => {
+    const post = await Post.create({
+      name: req.file?.originalname,
+      size: req.file?.size,
+      key: req.file?.filename,
+      url: "",
+    });
+
+    return res.send(post);
   }
 );
 
