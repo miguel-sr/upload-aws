e758afa4068f2bdae840e10cfb589358d46ec45e
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-02-02 21:45:22 -0300

    :sparkles: feat: Save file in AWS S3

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -1,14 +1,46 @@
+import { Express } from "express";
 import multer, { Options } from "multer";
 import path from "path";
 import crypto from "crypto";
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
-export default <Options>{
-  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: multer.diskStorage({
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SCRET_ACCESS_KEY;
+
+if (!accessKeyId || !secretAccessKey) {
+  throw new Error("Missing AWS credentials.");
+}
+
+interface MulterFile extends Express.MulterS3.File, Express.Multer.File {}
+
+const storageTypes = {
+  local: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
     },
-    filename: (req, file, cb) => {
+    filename: (req, file: MulterFile, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err, "");
+
+        file.key = `${hash.toString("hex")}-${file.originalname}`;
+
+        cb(null, file.key);
+      });
+    },
+  }),
+  s3: multerS3({
+    s3: new S3Client({
+      region: process.env.AWS_DEFAULT_REGION,
+      credentials: {
+        accessKeyId,
+        secretAccessKey,
+      },
+    }),
+    bucket: "miguelsrbucket",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
         if (err) cb(err, "");
 
@@ -18,6 +50,11 @@ export default <Options>{
       });
     },
   }),
+};
+
+export default <Options>{
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: storageTypes["s3"],
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,17 +6,24 @@ import Post from "./models/post";
 
 const routes = Router();
 
+interface MulterFile extends Express.MulterS3.File, Express.Multer.File {}
+
 routes.post(
   "/posts",
   multer(multerConfig).single("file"),
   async (req: Request, res: Response) => {
+    const {
+      originalname: name,
+      size,
+      location: url,
+      key,
+    } = req.file as MulterFile;
     const post = await Post.create({
-      name: req.file?.originalname,
-      size: req.file?.size,
-      key: req.file?.filename,
-      url: "",
+      name,
+      size,
+      url,
+      key,
     });
-
     return res.send(post);
   }
 );
